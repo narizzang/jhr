ff110bc7f714c3a9933c8e939915a5d80bdd6820
narizzang <hr_baby@naver.com>
2025-02-17 10:32:09 +0000

    script.js edited

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const projectsData = {
-    "project-1": ["hye-1.jpg", "hye-2.jpg", "hye-3.jpg", "hye-4.jpg", "hye-5.jpg", "hye-6.jpg", "hye-7.jpg"],
-    "project-2": ["eternal.jpg"],
-    "project-3": ["moonlight-1.jpg", "moonlight-2.jpg", "moonlight-3.jpg"]
+    "hye": ["hye-1.jpg", "hye-2.jpg", "hye-3.jpg", "hye-4.jpg", "hye-5.jpg", "hye-6.jpg", "hye-7.jpg"],
+    "eternal": ["eternal.jpg"],
+    "moonlight": ["moonlight-1.jpg", "moonlight-2.jpg", "moonlight-3.jpg"],
+    "postcard": ["postcard-1.jpg", "postcard-2.jpg"],
+    "visang": ["visang-1.jpg", "visang-2.jpg", "visang-3.jpg"],
+    "yjg": ["yjg-1.jpg", "yjg-2.jpg", "yjg-3.jpg", "yjg-4.jpg", "yjg-5.jpg", "yjg-6.jpg", "yjg-7.jpg", "yjg-8.jpg"]
 };
 
+
 document.addEventListener("DOMContentLoaded", function () {
-    const project1 = this.querySelector("#project1");
-    const content = this.querySelector(".content");
+    const content = document.querySelector(".content .content-inner");
 
     document.querySelectorAll(".work").forEach(work => {
       work.addEventListener("click", function () {
-        content.style.display = "block";
+        const projectId = this.id;
+        if (projectsData[projectId]) {
+            content.innerHTML = "";
+
+            projectsData[projectId].forEach(imgSrc => {
+                const project = document.createElement("div");
+                project.classList.add("project");
+                const img = document.createElement("img");
+                img.src = `project/${projectId}/${imgSrc}`;
+                project.appendChild(img);
+                content.appendChild(project);
+            });
+
+            content.style.display = "block";
+            content.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
       });
     });
-  });
+});
