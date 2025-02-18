b5a091942860a3743c8cd22ff84ff95fbea2296e
narizzang <hr_baby@naver.com>
2025-02-18 12:00:50 +0000

    content detail 수정

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (projectsData[projectId]) {
             contentTitle.innerHTML = "";
             contentDetail.innerHTML = "";
-            contentDetail.style.display = "block";
 
             var tmp = 0;
             detailData[projectId].forEach(des => {
